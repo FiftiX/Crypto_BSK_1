641199718b400b5698bd869a17d069a46691af81
FiftiX <fiftix@10g.pl>
2021-04-19 17:13:29 +0200

    Macierzowy 2a działa

--- src/project/Macierzowy2a.java
+++ src/project/Macierzowy2a.java
@@ -8,13 +8,57 @@ public class Macierzowy2a {
         this.text = text;
     }
 
-    public String encrypt(){
+    public String encrypt() {
+        char[] tab = text.toCharArray();
+        String result = "";
+        int param = tab.length;
 
-        return "2A CODE";
+        for (int i=0;i<param;i++){
+            for(int j=0;j<5;j++){
+                if (j == 0 && 5*i + 2 < param) {
+                    result += tab[5*i + 2];
+                }
+                if (j == 1 && 5*i + 3 < param) {
+                    result += tab[5*i + 3];
+                }
+                if (j == 2 && 5*i < param) {
+                    result += tab[5*i];
+                }
+                if (j == 3 && 5*i + 4 < param) {
+                    result += tab[5*i + 4];
+                }
+                if (j == 4 && 5*i + 1 < param) {
+                    result += tab[5*i + 1];
+                }
+            }
+        }
+        return result;
     }
 
     public String decrypt(){
+        char[] tab = text.toCharArray();
+        String result = "";
+        int param = tab.length;
 
-        return "2A DECODE";
+        for (int i=0;i<param;i++){
+            for(int j=0;j<5;j++){
+                if (j == 0 && 5*i + 2 < param) {
+                    result += tab[5*i + 2];
+                }
+                if (j == 1 && 5*i + 4 < param) {
+                    result += tab[5*i + 4];
+                }
+                if (j == 2 && 5*i < param) {
+                    result += tab[5*i];
+                }
+                if (j == 3 && 5*i + 1 < param) {
+                    result += tab[5*i + 1];
+                }
+                if (j == 4 && 5*i + 3 < param) {
+                    result += tab[5*i + 3];
+                }
+            }
+        }
+        return result;
     }
 }
