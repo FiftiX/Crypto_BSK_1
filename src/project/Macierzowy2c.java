b60731c67c946a8ed21727de0271cbc901db0e69
FiftiX <fiftix@10g.pl>
2021-04-22 22:15:23 +0200

    Macierzowy 2c działa

--- src/project/Macierzowy2c.java
+++ src/project/Macierzowy2c.java
@@ -8,13 +8,83 @@ public class Macierzowy2c {
         this.key = key;
     }
 
-    public String encrypt(){
+    public String encrypt() {
+        char[] tab = text.toCharArray();
+        char[] ktab = key.toCharArray();
+        int[] ltab = new int[ktab.length];
+        int[] stab = new int[ktab.length];
+        String result = "";
+        int pom = 0;
 
-        return "Macierzowy 2c code";
+        for (int i = 48; i < 123; i++) {
+            for (int j = 0; j < ktab.length; j++) {
+                if (ktab[j] == i)
+                    ltab[j] = pom++;
+            }
+        }
+
+        for (int i = 0; i < ltab.length; i++) {
+            for (int j = 0; j < ltab.length; j++) {
+                if (ltab[j] == i) {
+                    stab[i] = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < ktab.length; i++) {
+            int v = 0;
+            for (int j = 0; j < tab.length; j++) {
+                if (v < tab.length) {
+                    if (stab[i] <= stab[j] && (v+stab[i]< stab.length)) {
+                        result += tab[v + stab[i]];
+                    }
+                    v += stab[j] + 1;
+                } else {
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
     public String decrypt(){
+        char[] tab = text.toCharArray();
+        char[] ktab = key.toCharArray();
+        int[] ltab = new int[ktab.length];
+        int[] stab = new int[ktab.length];
+        int pom = 0;
 
-        return "Macierzowy 2c decode";
+        for (int i = 48; i < 123; i++) {
+            for (int j = 0; j < ktab.length; j++) {
+                if (ktab[j] == i)
+                    ltab[j] = pom++;
+            }
+        }
+
+        for (int i = 0; i < ltab.length; i++) {
+            for (int j = 0; j < ltab.length; j++) {
+                if (ltab[j] == i) {
+                    stab[i] = j;
+                }
+            }
+        }
+
+        char[] tab2 = new char[tab.length];
+        int num = 0;
+
+        for (int i = 0; i < ktab.length; i++) {
+            int v = 0;
+            for (int j = 0; j < tab.length; j++) {
+                if (v < tab.length) {
+                    if (stab[i] <= stab[j] && (v+stab[i]< stab.length)) {
+                        tab2[v + stab[i]] = tab[num++];
+                    }
+                    v += stab[j] + 1;
+                } else {
+                    break;
+                }
+            }
+        }
+        return String.valueOf(tab2);
     }
 }
