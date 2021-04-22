fc065fc6347287e45f5277d82393d17a1c84903d
FiftiX <fiftix@10g.pl>
2021-04-22 23:06:47 +0200

    Wszystkie Działają

--- src/project/Vigenerea.java
+++ src/project/Vigenerea.java
@@ -9,12 +9,55 @@ public class Vigenerea {
     }
 
     public String encrypt(){
+        char[] tab = text.toCharArray();
+        char[] ktab = key.toCharArray();
+        char[] ltab = new char[tab.length];
+        String result = "";
+        int j=0;
 
-        return "Vigenerea code";
+        for(int i=0;i<tab.length;i++){
+            ltab[i]=ktab[j];
+            j = j == ktab.length-1 ? 0 : j+1;
+        }
+
+        for(int i=0;i<tab.length;i++){
+            int pom = tab[i] + ltab[i];
+            if(pom<=155){
+                pom -= 65;
+                result += (char)pom;
+            }
+            else{
+                pom -= 91;
+                result += (char)pom;
+            }
+        }
+        return result;
     }
 
     public String decrypt(){
+        char[] tab = text.toCharArray();
+        char[] ktab = key.toCharArray();
+        char[] ltab = new char[tab.length];
+        String result = "";
+        int j=0;
 
-        return "Vigenerea decode";
+        for(int i=0;i<tab.length;i++){
+            ltab[i]=ktab[j];
+            j = j == ktab.length-1 ? 0 : j+1;
+        }
+
+        for(int i=0;i<tab.length;i++){
+            int pom = ltab[i] - tab[i];
+            if(pom<=0){
+                pom *= (-1);
+                pom += 65;
+                result += (char)pom;
+            }
+            else{
+                pom = 91 - pom;
+                result += (char)pom;
+            }
+        }
+        return result;
     }
 }
